1dddc85d96920ff67d34be0f0954ff51a005de6d
build <build@69e63612-969a-4371-9866-b14aae95a0cb>
2008-11-23 06:45:22 +0000

    Modifications module TestValidation (...)
    git-svn-id: http://projets.lucterios.org/svn/extensions/TestValidation@261 69e63612-969a-4371-9866-b14aae95a0cb

--- MachinTable_APAS_ValeurInitial.test.php
+++ MachinTable_APAS_ValeurInitial.test.php
@@ -18,14 +18,14 @@
 // 
 // 	Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 //  // Test file write by SDK tool
-// --- Last modification: Date 21 November 2008 12:14:06 By  ---
+// --- Last modification: Date 21 November 2008 14:53:45 By  ---
 
 
 //@TABLES@
 require_once('extensions/TestValidation/MachinTable.tbl.php');
 //@TABLES@
 
-//@DESC@
+//@DESC@valeur initial
 //@PARAM@ 
 
 function TestValidation_MachinTable_APAS_ValeurInitial(&$test)
@@ -34,6 +34,23 @@ function TestValidation_MachinTable_APAS_ValeurInitial(&$test)
 global $connect;
 $connect->execute("TRUNCATE TABLE TestValidation_MachinTable");
 try {
+	$Machin=new DBObj_TestValidation_MachinTable;
+	$test->assertEquals(0,$Machin->find(),"MachinTable nb 1");
+
+	require_once("CORE/extensionManager.inc.php");
+	$ext=new Extension('TestValidation','./extensions/TestValidation/');
+	$ret=$ext->upgradeDefaultValueTable();
+	$msg=$ext->message;
+	$test->assertEquals(true,$ret,"upgradeDefaultValueTable 1:$msg");
+	$Machin=new DBObj_TestValidation_MachinTable;
+	$test->assertEquals(3,$Machin->find(),"MachinTable nb 2:".$ext->message);
+
+	$ext=new Extension('TestValidation','./extensions/TestValidation/');
+	$ret=$ext->upgradeDefaultValueTable();
+	$msg=$ext->message;
+	$test->assertEquals(true,$ret,"upgradeDefaultValueTable 2:$msg");
+	$Machin=new DBObj_TestValidation_MachinTable;
+	$test->assertEquals(3,$Machin->find(),"MachinTable nb 3:".$ext->message);
 
 	$connect->execute("TRUNCATE TABLE TestValidation_MachinTable");
 } catch(Exception $e) {
--- setup.inc.php
+++ setup.inc.php
@@ -18,7 +18,7 @@
 // 
 // 	Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 //  // setup file write by SDK tool
-// --- Last modification: Date 21 November 2008 12:14:06 By  ---
+// --- Last modification: Date 21 November 2008 14:53:45 By  ---
 
 $extention_name="TestValidation";
 $extention_description="TestValidation";
@@ -30,7 +30,7 @@ $extension_libre=true;
 $version_max=0;
 $version_min=1;
 $version_release=4;
-$version_build=8;
+$version_build=20;
 
 $depencies=array();
 $depencies[0] = new Param_Depencies("CORE", 0, 16, 0, 16, false);
