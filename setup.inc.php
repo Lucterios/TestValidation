e68ca671caf9b45d6f13e05abf5a84a57263c8a9
build <build@69e63612-969a-4371-9866-b14aae95a0cb>
2009-05-06 19:12:31 +0000

    Modifications module TestValidation (...)
    git-svn-id: http://projets.lucterios.org/svn/extensions/TestValidation@450 69e63612-969a-4371-9866-b14aae95a0cb

--- setup.inc.php
+++ setup.inc.php
@@ -18,7 +18,7 @@
 // 
 // 	Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 //  // setup file write by SDK tool
-// --- Last modification: Date 05 May 2009 0:05:13 By  ---
+// --- Last modification: Date 05 May 2009 7:58:20 By  ---
 
 $extention_name="TestValidation";
 $extention_description="TestValidation";
@@ -29,8 +29,8 @@ $extension_libre=true;
 
 $version_max=0;
 $version_min=3;
-$version_release=2;
-$version_build=82;
+$version_release=89;
+$version_build=92;
 
 $depencies=array();
 $depencies[0] = new Param_Depencies("CORE", 0, 93, 0, 90, false);
@@ -64,39 +64,45 @@ $actions[4] = new Param_Action("Supprimer un MachinTable", "MachinTable_APAS_Del
 $actions[5] = new Param_Action("Fiche d'un MachinTable", "MachinTable_APAS_Fiche", 0);
 $actions[6] = new Param_Action("Lister des MachinTable", "MachinTable_APAS_List", 0);
 $actions[7] = new Param_Action("Nouveau composants", "NewComponent", 0);
-$actions[8] = new Param_Action("Valider un SuperTableTest", "SuperTableTest_APAS_AddModifyAct", 0);
-$actions[9] = new Param_Action("Ajouter/Modifier un SuperTableTest", "SuperTableTest_APAS_AddModify", 0);
-$actions[10] = new Param_Action("Supprimer un SuperTableTest", "SuperTableTest_APAS_Del", 0);
-$actions[11] = new Param_Action("Fiche d'un SuperTableTest", "SuperTableTest_APAS_Fiche", 0);
-$actions[12] = new Param_Action("Lister des SuperTableTest", "SuperTableTest_APAS_List", 0);
-$actions[13] = new Param_Action("Rechercher un SuperTableTest", "SuperTableTest_APAS_Search", 0);
-$actions[14] = new Param_Action("Erreurs en Base", "TableTest_APAS_ErrorInTable", 0);
-$actions[15] = new Param_Action("Lister des TableTest", "TableTest_APAS_List", 0);
-$actions[16] = new Param_Action("Rechercher un TableTest", "TableTest_APAS_Search", 0);
-$actions[17] = new Param_Action("Truc", "TableTest_APAS_Truc", 0);
-$actions[18] = new Param_Action("Valider une erreur", "TableTest_APAS_ValiderErreur", 0);
-$actions[19] = new Param_Action("Ajouter", "TableTest_APAS_ajouter", 0);
-$actions[20] = new Param_Action("Editer", "TableTest_APAS_editer", 0);
-$actions[21] = new Param_Action("Ecran des erreurs", "TableTest_APAS_errorManager", 0);
-$actions[22] = new Param_Action("Supprimer", "TableTest_APAS_suppr", 0);
-$actions[23] = new Param_Action("Valider", "TableTest_APAS_valider", 0);
-$actions[24] = new Param_Action("verouxDlg", "TableTest_APAS_verouxDlg", 0);
-$actions[25] = new Param_Action("verouxSimple", "TableTest_APAS_verouxSimple", 0);
-$actions[26] = new Param_Action("veroux", "TableTest_APAS_veroux", 0);
-$actions[27] = new Param_Action("Action de validation des composants", "TestComposants", 0);
-$actions[28] = new Param_Action("Valider un TrucTable", "TrucTable_APAS_AddModifyAct", 0);
-$actions[29] = new Param_Action("Ajouter/Modifier un TrucTable", "TrucTable_APAS_AddModify", 0);
-$actions[30] = new Param_Action("Supprimer un TrucTable", "TrucTable_APAS_Del", 0);
-$actions[31] = new Param_Action("Fiche d'un TrucTable", "TrucTable_APAS_Fiche", 0);
-$actions[32] = new Param_Action("Lister des TrucTable", "TrucTable_APAS_List", 0);
-$actions[33] = new Param_Action("Téléchargement", "UpAndDownLoad", 0);
-$actions[34] = new Param_Action("", "selectContact", 0);
+$actions[8] = new Param_Action("Valider un SuperContact", "SuperContact_APAS_AddModifyAct", 0);
+$actions[9] = new Param_Action("Ajouter/Modifier un SuperContact", "SuperContact_APAS_AddModify", 0);
+$actions[10] = new Param_Action("Supprimer un SuperContact", "SuperContact_APAS_Del", 0);
+$actions[11] = new Param_Action("Fiche d'un SuperContact", "SuperContact_APAS_Fiche", 0);
+$actions[12] = new Param_Action("Lister des SuperContact", "SuperContact_APAS_List", 0);
+$actions[13] = new Param_Action("Valider un SuperTableTest", "SuperTableTest_APAS_AddModifyAct", 0);
+$actions[14] = new Param_Action("Ajouter/Modifier un SuperTableTest", "SuperTableTest_APAS_AddModify", 0);
+$actions[15] = new Param_Action("Supprimer un SuperTableTest", "SuperTableTest_APAS_Del", 0);
+$actions[16] = new Param_Action("Fiche d'un SuperTableTest", "SuperTableTest_APAS_Fiche", 0);
+$actions[17] = new Param_Action("Lister des SuperTableTest", "SuperTableTest_APAS_List", 0);
+$actions[18] = new Param_Action("Rechercher un SuperTableTest", "SuperTableTest_APAS_Search", 0);
+$actions[19] = new Param_Action("Erreurs en Base", "TableTest_APAS_ErrorInTable", 0);
+$actions[20] = new Param_Action("Lister des TableTest", "TableTest_APAS_List", 0);
+$actions[21] = new Param_Action("Rechercher un TableTest", "TableTest_APAS_Search", 0);
+$actions[22] = new Param_Action("Truc", "TableTest_APAS_Truc", 0);
+$actions[23] = new Param_Action("Valider une erreur", "TableTest_APAS_ValiderErreur", 0);
+$actions[24] = new Param_Action("Ajouter", "TableTest_APAS_ajouter", 0);
+$actions[25] = new Param_Action("Editer", "TableTest_APAS_editer", 0);
+$actions[26] = new Param_Action("Ecran des erreurs", "TableTest_APAS_errorManager", 0);
+$actions[27] = new Param_Action("Supprimer", "TableTest_APAS_suppr", 0);
+$actions[28] = new Param_Action("Valider", "TableTest_APAS_valider", 0);
+$actions[29] = new Param_Action("verouxDlg", "TableTest_APAS_verouxDlg", 0);
+$actions[30] = new Param_Action("verouxSimple", "TableTest_APAS_verouxSimple", 0);
+$actions[31] = new Param_Action("veroux", "TableTest_APAS_veroux", 0);
+$actions[32] = new Param_Action("Action de validation des composants", "TestComposants", 0);
+$actions[33] = new Param_Action("Valider un TrucTable", "TrucTable_APAS_AddModifyAct", 0);
+$actions[34] = new Param_Action("Ajouter/Modifier un TrucTable", "TrucTable_APAS_AddModify", 0);
+$actions[35] = new Param_Action("Supprimer un TrucTable", "TrucTable_APAS_Del", 0);
+$actions[36] = new Param_Action("Fiche d'un TrucTable", "TrucTable_APAS_Fiche", 0);
+$actions[37] = new Param_Action("Lister des TrucTable", "TrucTable_APAS_List", 0);
+$actions[38] = new Param_Action("Téléchargement", "UpAndDownLoad", 0);
+$actions[39] = new Param_Action("", "selectContact", 0);
 
 $params=array();
 
 $extend_tables=array();
 $extend_tables["AutreTable"] = array("TestValidation.AutreTable","",array("TestValidation_TableTest"=>"test",));
 $extend_tables["MachinTable"] = array("TestValidation.MachinTable","",array("TestValidation_TrucTable"=>"truc",));
+$extend_tables["SuperContact"] = array("Super-contact","org_lucterios_contacts/personnePhysique",array());
 $extend_tables["SuperTableTest"] = array("Super table test","TestValidation/TableTest",array());
 $extend_tables["TableTest"] = array("TestValidation.TableTest","",array());
 $extend_tables["TrucTable"] = array("TestValidation.TrucTable","",array("TestValidation_SuperTableTest"=>"superTest",));
